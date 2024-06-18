917527603324a6d5635b53a05488e62a53f06967
Aarin06 <aarinjasikumar@gmail.com>
2024-06-18 11:53:29 -0400

    test

--- backend/api/index.js
+++ backend/api/index.js
@@ -32,20 +32,27 @@ app.use(cors({
 
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Use the origin from the request
+  const allowedOrigins = ['https://true-north-pickleball-front.vercel.app']; // Add other allowed origins if needed
+  const origin = req.headers.origin;
+  
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
   res.setHeader(
     'Access-Control-Allow-Headers',
     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
+  );
+
   if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
+    res.status(200).end();
+    return;
   } else {
-    next()
+    next();
   }
 });
 
