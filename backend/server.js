61786b8a6d14f0d88292fca1c8d9076646a3cd76
Aarin06 <aarinjasikumar@gmail.com>
2024-06-18 09:47:17 -0400

    test

--- backend/server.js
+++ backend/server.js
@@ -25,11 +25,29 @@ app.use(session({
 
 // middleware
 app.use(express.json());
-app.use(cors({
-  origin: 'https://true-north-pickleball-front.vercel.app',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-}));
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+const handler = (req, res) => {
+  const d = new Date()
+  res.end(d.toString())
+}
+
+app.use(allowCors(handler))
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
