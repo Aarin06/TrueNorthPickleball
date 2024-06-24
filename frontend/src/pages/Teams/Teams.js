6f3fb62b3083aed107d09c43f9ef43127e86f3f6
Aarin06 <aarinjasikumar@gmail.com>
2024-06-24 11:20:38 -0400

    add loading animation for teams

--- frontend/src/pages/Teams/Teams.js
+++ frontend/src/pages/Teams/Teams.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Button, Container, Grid, Card, Typography, Box } from "@mui/material";
+import { Button, Container, Grid, Card, Typography, Box, CircularProgress } from "@mui/material";
 import { Link } from "react-router-dom";
 import { experienceMapping } from "../../Mappings/experienceLevel";
 import { getTeams } from "../../api/teamService";
@@ -7,10 +7,13 @@ import "tailwindcss/tailwind.css";
 
 function Teams() {
   const [teams, setTeams] = useState([]);
+  const [isLoading, setIsLoading] = useState(true)
+
 
   useEffect(() => {
     getTeams().then((response) => {
       setTeams(response);
+      setIsLoading(false);
     });
   }, []);
 
@@ -22,35 +25,41 @@ function Teams() {
         </Typography>
       </Box>
       
-      <Box className="mb-8">
-        {Object.entries(experienceMapping).map(([key, value]) => (
-          <Box key={key} className="mb-8">
-            <Card elevation={10} className="p-4 mb-4 min-h-20">
-              <Typography variant="h3"sx={{marginBottom:"10px"}}>
-                {value}
-              </Typography>
-              <Grid container spacing={2}>
-                {teams
-                  .filter((team) => team.experienceLevel == key)
-                  .map((team) => (
-                    <Grid item xs={6} sm={6} md={4} key={team._id}>
-                      <Card
-                        component={Link}
-                        to={`/teams/${team._id}`}
-                        sx={{ backgroundColor: "#ac1121" }}
-                        className="h-auto p-10 flex flex-col justify-center items-center"
-                      >
-                        <Typography variant="h6" className="text-white text-center">
-                          {team.name}
-                        </Typography>
-                      </Card>
-                    </Grid>
-                  ))}
-              </Grid>
-            </Card>
-          </Box>
-        ))}
-      </Box>
+      {isLoading ? 
+        <div className="flex justify-center mt-20">
+          <CircularProgress size={100} />
+        </div>
+      :
+        <Box className="mb-8">
+          {Object.entries(experienceMapping).map(([key, value]) => (
+            <Box key={key} className="mb-8">
+              <Card elevation={10} className="p-4 mb-4 min-h-20">
+                <Typography variant="h3"sx={{marginBottom:"10px"}}>
+                  {value}
+                </Typography>
+                <Grid container spacing={2}>
+                  {teams
+                    .filter((team) => team.experienceLevel == key)
+                    .map((team) => (
+                      <Grid item xs={6} sm={6} md={4} key={team._id}>
+                        <Card
+                          component={Link}
+                          to={`/teams/${team._id}`}
+                          sx={{ backgroundColor: "#ac1121" }}
+                          className="h-auto p-10 flex flex-col justify-center items-center"
+                        >
+                          <Typography variant="h6" className="text-white text-center">
+                            {team.name}
+                          </Typography>
+                        </Card>
+                      </Grid>
+                    ))}
+                </Grid>
+              </Card>
+            </Box>
+          ))}
+        </Box>
+      }
     </Container>
   );
 }
